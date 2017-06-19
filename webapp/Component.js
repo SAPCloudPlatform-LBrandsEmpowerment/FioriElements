fdf604c214fa096e31df7a13d6a30a8baa51b474
i077837 <vivekananda.panigrahy@sap.com>
2017-06-19 14:37:24 +0000

    Latest code

---
+++ webapp/Component.js
@@ -0,0 +1,9 @@
+jQuery.sap.declare("com.lbrands.cloudconnectorcloudconnectordemo.Component");
+sap.ui.getCore().loadLibrary("sap.ui.generic.app");
+jQuery.sap.require("sap.ui.generic.app.AppComponent");
+
+sap.ui.generic.app.AppComponent.extend("com.lbrands.cloudconnectorcloudconnectordemo.Component", {
+	metadata: {
+		"manifest": "json"
+	}
+});
